4ee333e90d69256e730f7ba4ec6e4ed39f565bed
rpnanhai <rpnanhai@gmail.com>
2016-12-20 21:53:00 +0800

    feat:增加单元测试和CI bugfix

--- src/huami.php
+++ src/huami.php
@@ -11,9 +11,25 @@
 
 namespace HuaMi;
 
-class HuaMi
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HuaMi extends Command
 {
-    public function __construct(){
+    public function __construct($msg)
+    {
+        $this->msg = $msg;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('test');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<comment>' . $this->msg . '</comment>');
     }
 }
-
---
+++ tests/huami/HuaMiTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace HuaMi\Tests;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use HuaMi\HuaMi;
+
+
+class HuaMiTests extends \PHPUnit_Framework_TestCase
+{
+    public function testExecute() {
+        $application = new Application();
+        $application->add(new HuaMi("aaa"));
+        $command = $application->find('test');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => 'aaa'));
+        $this->assertRegExp('<comment>aaa</comment>', $commandTester->getDisplay());
+    }
+}
