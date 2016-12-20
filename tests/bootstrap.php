3a786e98b5d094ef74da100378d3c58163c1bec6
rpnanhai <rpnanhai@gmail.com>
2016-12-20 22:30:23 +0800

    fix: phpunit 文件大小写问题

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,4 +8,5 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
 require __DIR__ . '/../vendor/autoload.php';
--- tests/huami/HuaMiTest.php
+++ tests/huami/HuaMiTest.php
@@ -1,19 +1,29 @@
 <?php
+
+/*
+ * The file is part of the rpnanhai/HuaMi
+ *
+ * (c) 2016 rpnanhai <rpnanhai@gamil.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace HuaMi\Tests;
 
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 use HuaMi\HuaMi;
 
-
 class HuaMiTests extends \PHPUnit_Framework_TestCase
 {
-    public function testExecute() {
+    public function testExecute()
+    {
         $application = new Application();
-        $application->add(new HuaMi("aaa"));
-        $command = $application->find('test');
+        $application->add(new HuaMi('aaa'));
+        $command       = $application->find('test');
         $commandTester = new CommandTester($command);
-        $commandTester->execute(array('command' => 'aaa'));
+        $commandTester->execute(['command' => 'aaa']);
         $this->assertRegExp('//', $commandTester->getDisplay());
     }
 }
